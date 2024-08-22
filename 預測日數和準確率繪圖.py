8f87430b76422a3dfb2e8467e3b06c6c204268b2
Li732375 <lee732375@gmail.com>
2024-08-22 16:16:41 +0800

    Update 預測日數和準確率繪圖.py
    資料檔案路徑更新、程式碼格式微調、調整排除指定期間表示寫法、新增當前第幾模型訓練序位檢視、新增繪圖最大最小值顯示、修正水平軸刻度不對齊實際數據

--- 預測日數和準確率繪圖.py
+++ 預測日數和準確率繪圖.py
@@ -2,13 +2,9 @@ import pandas as pd
 import talib
 
 
-Currency_data = pd.read_excel('TWD%3DX_Currency_data.xlsx', 
+DataFolder = 'Ori_Data/'
+Currency_data = pd.read_excel(DataFolder + 'USDtoTWD_Currency_data.xlsx', 
                               index_col = 'Date')  # 讀取匯率資料
-
-missing_values = Currency_data.isnull().sum() # 檢查每一列是否有空值
-
-#print(missing_values)
-
 Currency_data.drop(columns = ['Adj Close'], inplace = True)
 df_open = Currency_data['Open']
 df_close = Currency_data['Close']
@@ -44,10 +40,11 @@ Currency_data['WILLR'] = talib.WILLR(df_high, df_low, df_close,
 Currency_data['SAR'] = talib.SAR(df_high, df_low)
 Currency_data['AVGPRICE'] = talib.AVGPRICE(df_open, df_high, df_low, df_close)
 Currency_data['WCLPRICE'] = talib.WCLPRICE(df_high, df_low, df_close)
-Currency_data['LINEARREG_ANGLE'] = talib.LINEARREG_ANGLE(df_close,14)
-Currency_data['WMA'] = talib.WMA(df_close,30) # 計算 MA5
-Currency_data['STDDEV'] = talib.STDDEV (df_close, timeperiod=5, nbdev=1)
-Currency_data['CDL3BLACKCROWS'] = talib.CDL3BLACKCROWS (df_open, df_high, df_low, df_close)
+Currency_data['LINEARREG_ANGLE'] = talib.LINEARREG_ANGLE(df_close, 14)
+Currency_data['WMA'] = talib.WMA(df_close, 30) # 計算 MA5
+Currency_data['STDDEV'] = talib.STDDEV (df_close, timeperiod = 5, nbdev = 1)
+Currency_data['CDL3BLACKCROWS'] = talib.CDL3BLACKCROWS (df_open, df_high, 
+                                                        df_low, df_close)
 
 
 columns_to_shift = ['Close', 'MA_5', 'MA_10', 'MA_20', 'RSI_14', 'MACD', 
@@ -67,23 +64,22 @@ columns_to_shift = ['Close', 'MA_5', 'MA_10', 'MA_20', 'RSI_14', 'MACD',
 
 
 # 因資料特定欄位計算有回朔需求而向前推進抓取時間，設定要排除的期間
-start_date = '2019-01-01'
 end_date   = '2019-12-31'
 
 # 排除特定期間內的數據
 Currency_data.drop(Currency_data.
-                   loc[start_date : end_date].index, inplace = True)
+                   loc[:end_date].index, inplace = True)
 print(Currency_data.head())
 
 # 讀入其他資料進行合併
-Fed_Funds_Rate = pd.read_excel('Fed_Funds_Rate.xlsx')  
-USA_CPI = pd.read_excel('USA_CPI_Data.xlsx')  
-USA_Unemployment_Rate = pd.read_excel('USA_Unemployment_Rate.xlsx')  
-TW_CPI = pd.read_excel('TW_CPI.xlsx')
-USA_GDP = pd.read_excel('USA_GDP.xlsx')
-TW_Rate = pd.read_excel('TW_Rate.xlsx')
-DXY_NYB = pd.read_excel('dxy_data.xlsx')
-GOLD_data = pd.read_excel('gold_data.xlsx')
+Fed_Funds_Rate = pd.read_excel(DataFolder + 'Fed_Funds_Rate.xlsx')  
+USA_CPI = pd.read_excel(DataFolder + 'USA_CPI_Data.xlsx')  
+USA_Unemployment_Rate = pd.read_excel(DataFolder + 'USA_Unemployment_Rate.xlsx')  
+TW_CPI = pd.read_excel(DataFolder + 'TW_CPI.xlsx')
+USA_GDP = pd.read_excel(DataFolder + 'USA_GDP.xlsx')
+TW_Rate = pd.read_excel(DataFolder + 'TW_Rate.xlsx')
+DXY_NYB = pd.read_excel(DataFolder + 'Dx-y_data.xlsx')
+GOLD_data = pd.read_excel(DataFolder + 'Gold_data.xlsx')
 
 # =============================================================================
 # merge_asof，用於合併兩個數據框，其中一個數據框的時間戳（或排序列）可能在另一個數據框中找不到完全對應的記錄。這時，可以根據時間戳的前向或後向對齊進行合併。
@@ -153,7 +149,7 @@ def function(num): # 預測日數
     
 test_list = [0]
     
-for i in range(30):
+for i in range(60):
     function(i + 1)
     
     #print(df_merge.head())
@@ -175,22 +171,21 @@ for i in range(30):
     df = pd.read_excel('data.xlsx')
 
     #print(df.columns)
-    feature_names = ['Close_y', 'High_y', 'CPIAUCNS', 'Open_y', 'UNRATE', 'MA_20', 
-                     'MA_10', 'Growth Rate_x', 'TW_CPI_Rate', 
+    feature_names = ['Close_y', 'High_y', 'CPIAUCNS', 'Open_y', 'UNRATE', 
+                     'MA_20', 'MA_10', 'Growth Rate_x', 'TW_CPI_Rate', 
                      'WILLR', 'Open_x', 'K', 'RSI_14', 'Volume_y', 
                      'Growth Rate_y', 'FEDFUNDS', 'Bollinger Bands lower', 
                      'Bollinger Bands Upper', 'USA_GDP_Rate']
     # 0.821
         
-    def split_stock_data(stock_data, label_column, delete_column, test_size = 0.3, 
-                         random_state = 42):
+    def split_stock_data(stock_data, label_column, delete_column, 
+                         test_size = 0.3, random_state = 42):
         X = stock_data[feature_names].values
         y = stock_data[label_column].values
         
-        X_train, X_test, y_train, y_test = train_test_split(X, y, 
-                                                            test_size = test_size, 
-                                                            random_state = 
-                                                            random_state)
+        X_train, X_test, y_train, y_test = \
+            train_test_split(X, y, test_size = test_size, 
+                             random_state = random_state)
 
         return X_train, X_test, y_train, y_test, feature_names
 
@@ -199,8 +194,8 @@ for i in range(30):
     accuracies = []
 
     # 分割資料
-    trainX, testX, trainY, testY, feature_names = split_stock_data(df, label_column, 
-                                                    delete_column)
+    trainX, testX, trainY, testY, feature_names = \
+        split_stock_data(df, label_column, delete_column)
     Xgboost = XGBClassifier()
     start_time = time.time()
     Xgboost.fit(trainX, trainY)
@@ -209,7 +204,7 @@ for i in range(30):
     test_predic = Xgboost.predict(testX) # 取得預測的結果
     test_acc = Xgboost.score(testX, testY)
 
-    print('Xgboost測試集準確率 %.3f' % test_acc)
+    print(f'Xgboost({i + 1}) 測試集準確率 %.3f' % test_acc)
     print(f"訓練時間: {training_time // 60:.2f} 分 {training_time % 60:.2f} 秒")
     # 0.821
     
@@ -219,12 +214,48 @@ import matplotlib.pyplot as plt
 
 plt.rcParams['font.family'] = 'Microsoft JhengHei' # 設置中文字體
 plt.figure(figsize = (12, 6))
-a = pd.DataFrame(test_list).plot(kind = 'line', color = 'green')
-plt.title('預測日數準確度')
-plt.xlabel('日數')
-plt.ylabel('準確率')
+all_acc = pd.Series(test_list)
+a = all_acc.iloc[1:].plot(kind = 'line', color = 'green')
+ytick = [i / 20 for i in range(10, 20)] # 注意到 range 的全部參數僅能輸入整數，因此要輸出小數數列需換個形式
+plt.yticks(ytick, [str(int(i * 100)) + ' %' for i in ytick])
+plt.ylim(0.55, 1.0)
+
+# 在圖上標註最大值
+plt.annotate(f'{all_acc.idxmax()}日 ({all_acc.max() * 100:.1f} %)',
+             xy = (all_acc.idxmax(), all_acc.max()),  # 註釋點的位置
+             xytext = (all_acc.idxmax(), all_acc.max() - 0.1),  # 註釋文字的位置
+             arrowprops = dict(facecolor = 'red', shrink = 0.2, 
+                               headlength = 16),  # 設置箭頭的屬性
+             fontsize = 16,
+             color = 'red',
+             ha ='center')
+# =============================================================================
+# arrowprops 參數
+# facecolor：箭頭的填充顏色。這裡設為 'red'。
+# edgecolor：箭頭邊緣的顏色。這裡設為 'black'。
+# shrink：箭頭縮小比例。值為 0.1 時箭頭會比正常情況長，根據需要調整。
+# width：箭頭線條的寬度。值為 1 時箭頭線條寬度為 1 個像素。
+# headwidth：箭頭頭部的寬度。增加此值會使箭頭頭部更寬。
+# headlength：箭頭頭部的長度。增加此值會使箭頭頭部更長。
+# =============================================================================
+
+
+# 在圖上標註最小值
+plt.annotate(f'{all_acc.iloc[1:].idxmin()}日 ({all_acc.iloc[1:].min() * 100:.1f} %)',
+             xy = (all_acc.iloc[1:].idxmin(), all_acc.iloc[1:].min()),  # 註釋點的位置
+             xytext = (all_acc.iloc[1:].idxmin() + 5, all_acc.iloc[1:].min()),  # 註釋文字的位置
+             arrowprops = dict(facecolor = 'blue', shrink = 0.2, 
+                               headlength = 16),  # 設置箭頭的屬性
+             fontsize = 16,
+             color = 'blue',
+             va = 'center')
+
+plt.title(f'預測日數準確度折線圖 【訓練資料期間 {str(df_merge['DATE'][0])[:10]} - {str(df_merge['DATE'][len(df_merge) - 1])[:10]}】', 
+          fontsize = 18)
+plt.xlabel('日數', fontsize = 15)
+plt.ylabel('準確率', fontsize = 15)
 plt.grid(axis = 'y')
-plt.legend(['模型準確率'])
+plt.legend(['準確率'], fontsize = 15)
     
 
 
