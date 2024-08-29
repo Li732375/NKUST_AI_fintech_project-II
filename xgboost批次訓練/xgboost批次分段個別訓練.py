63e69686bfd159b4a0c31831af700cd909e210e4
Li732375 <lee732375@gmail.com>
2024-08-29 14:17:23 +0800

    Update xgboost批次分段個別訓練.py
    應繪圖功能獨立，更新程式碼

--- xgboost批次訓練/xgboost批次分段個別訓練.py
+++ xgboost批次訓練/xgboost批次分段個別訓練.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import TimeSeriesSplit
 import math
 from xgboost import XGBClassifier
 import time
-import matplotlib.pyplot as plt
+import Plot
 
 # 讀取資料
 df = pd.read_excel("../data.xlsx")
@@ -102,59 +102,7 @@ def calculate_overlap_rate(indices_list):
 overlap_rate = calculate_overlap_rate(split_indices)
 print(f"資料彼此重疊率：{overlap_rate * 100:.2f} %")
 
-# 顯示每批準確度變化
-plt.rcParams['font.family'] = 'Microsoft JhengHei' # 設置中文字體
 
-# 設置背景顏色
-plt.gcf().patch.set_facecolor('black')  # 設置整個圖表背景為黑色
-plt.gca().set_facecolor('black')  # 設置坐標軸背景為黑色
-
-# =============================================================================
-# fig, ax = plt.subplots()
-# fig.patch.set_facecolor('black')  # 設置整個圖表背景為黑色
-# ax.set_facecolor('black')  # 設置坐標軸背景為黑色
-# =============================================================================
-
-plt.plot(range(1, len(batch_test_scores) + 1), batch_test_scores, 
-         label = '批次測試集準確率', color = 'blue')
-plt.plot(range(1, len(batch_test_scores) + 1), latest_test_scores, 
-         label = '最新數據準確率', color = 'lime')
-plt.xticks([i for i in range(1, len(batch_test_scores) + 1)], color = 'white')
-plt.xlabel("序位（批）", color = 'white')
-ytick = [i / 100 for i in range(0, 105, 10)]
-plt.yticks(ytick, [str(int(i * 100)) + ' %' for i in ytick], color = 'white')
-plt.ylabel("準確率", color = 'white')
-plt.title("分批個別訓練 - 準確率", color = 'white') 
-plt.grid(True, axis = 'y')
-plt.legend(loc = 'lower left', facecolor = 'black', labelcolor = 'w')
-
-# =============================================================================
-# 訓練集和測試集區間的橫條圖
-plt.figure(figsize=(12, 6))
-plt.gcf().patch.set_facecolor('black')  # 設置整個圖表背景為黑色
-plt.gca().set_facecolor('black')  # 設置坐標軸背景為黑色
-
-# 繪製橫條圖
-for i, (train_index, test_index) in enumerate(TSS.split(X, y)):
-    train_start = min(train_index)
-    train_end = max(train_index)
-    test_start = min(test_index)
-    test_end = max(test_index)
-    
-    plt.barh(i + 1, train_end - train_start + 1, left = train_start, 
-             color = 'blue', edgecolor = 'white', 
-             label = '訓練集' if i == 0 else "")
-    plt.barh(i + 1, test_end - test_start + 1, left = test_start, 
-             color = 'red', edgecolor = 'white', 
-             label = '測試集' if i == 0 else "")
-
-plt.xlim(-1, len(df) + 0.5)
-plt.xticks(color = 'white')
-plt.xlabel("資料索引", color = 'white', fontsize = 11)
-plt.ylim(1, n_splits + 1)
-plt.yticks(range(n_splits + 1), [''] + [str(i + 1) for i in range(n_splits)], 
-           color = 'white')
-plt.ylabel("批次", color = 'white', fontsize = 11)
-plt.title("批次區間", color = 'white', fontsize = 14)
-plt.legend(loc='upper left', facecolor = 'black', labelcolor = 'white')
-plt.grid(True, axis = 'x', color = 'white', linestyle = '--', linewidth = 0.5)
+# 繪製圖形
+Plot.AccLineAndDataArea_Draw(batch_test_scores, latest_test_scores, 
+                             TSS.split(X, y), len(df), n_splits)
