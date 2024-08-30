906784c89e4a657d3cfee2aba6a9822b83222a03
Li732375 <lee732375@gmail.com>
2024-08-30 15:34:11 +0800

    Update 資料下載.py
    拓展資料來源上限

--- 資料下載.py
+++ 資料下載.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import pandas as pd
 
-Data_Time_Start = '2018-01-01' # 應後續特徵處理時避免缺值，必須再提前目標一年
+Data_Time_Start = '2003-01-01' # 應後續特徵處理時避免缺值，必須再提前目標一年
 Data_Time_End = '2023-12-31'
 Data_Time_TW_Start = str(int(Data_Time_Start[0 : 4]) - 1911) + '-01-01'
 Data_Time_TW_End = str(int(Data_Time_End[0 : 4]) - 1911) + '-12-31'
