1b18ed2c918736a544b09b1af028721244873d2f
Li732375 <lee732375@gmail.com>
2024-08-30 11:38:03 +0800

    Update Plot.py
    新增資料圓點顯示、水平軸刻度顯示修正、水平軸頭尾刻度顯示、水平軸名稱更新、水平軸與圖例文字尺寸設定

--- xgboost訓練/Plot.py
+++ xgboost訓練/Plot.py
@@ -10,23 +10,29 @@ def AccLineAndDataArea_Draw(title, batch_test_scores, latest_test_scores, TSSspl
     plt.gca().set_facecolor('black')  # 設置坐標軸背景為黑色
     
     # =============================================================================
+    # 同效果方法二
     # fig, ax = plt.subplots()
     # fig.patch.set_facecolor('black')  # 設置整個圖表背景為黑色
     # ax.set_facecolor('black')  # 設置坐標軸背景為黑色
     # =============================================================================
     
-    plt.plot(range(1, len(batch_test_scores) + 1), batch_test_scores, 
+    plt.plot(range(1, len(batch_test_scores) + 1), batch_test_scores, marker = 'o',
              label = '批次測試集準確率', color = 'blue')
-    plt.plot(range(1, len(batch_test_scores) + 1), latest_test_scores, 
+    plt.plot(range(1, len(batch_test_scores) + 1), latest_test_scores, marker = 'o',
              label = '最新數據測試準確率', color = 'lime')
-    plt.xticks([i for i in range(1, len(batch_test_scores) + 1)], color = 'white')
-    plt.xlabel("序位（批）", color = 'white')
+    plt.xticks([1, 5] + [i for i in range(10, len(batch_test_scores), 5)] + \
+               [len(batch_test_scores)], 
+               ['1'] + [str(i) for i in range(5, len(batch_test_scores), 5)] + \
+                   [len(batch_test_scores)],
+               color = 'white')
+    plt.xlabel("批次順序", color = 'white')
     ytick = [i / 100 for i in range(0, 105, 10)]
     plt.yticks(ytick, [str(int(i * 100)) + ' %' for i in ytick], color = 'white')
     plt.ylabel("準確率", color = 'white')
-    plt.title(title + ' - 準確率', color = 'white') 
-    plt.grid(True, axis = 'y')
-    plt.legend(loc = 'lower left', facecolor = 'black', labelcolor = 'w')
+    plt.title(title + ' - 準確率', color = 'white', fontsize = 14) 
+    plt.grid(True)
+    plt.legend(loc = 'lower left', facecolor = 'black', labelcolor = 'w', 
+               fontsize = 10)
     
     # =============================================================================
     # 訓練集和測試集區間的橫條圖
