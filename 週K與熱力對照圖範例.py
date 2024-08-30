89c0f4b4bf30c88df0f48bcb9b8ca0456f05bd6d
Li732375 <lee732375@gmail.com>
2024-08-30 12:24:19 +0800

    Update 週K與熱力對照圖範例.py

--- 週K與熱力對照圖範例.py
+++ 週K與熱力對照圖範例.py
@@ -151,7 +151,7 @@ def HeatmapAndWeeklyCandlestick_darw(testX, feature_names):
 
     plt.xlim(-0.5, len(KLine_df) - 0.5) # 調整距離，與下圖對應
     plt.xticks([0, 4] + [i for i in range(9, len(KLine_df), 5)], 
-               [1 if i == 0 else i for i in range(0, len(KLine_df), 5)],) # 不可隱藏，會影響網格直線的繪製
+               [1 if i == 0 else i for i in range(0, len(KLine_df), 5)]) # 不可隱藏，會影響網格直線的繪製
     plt.yticks(fontsize = 10, color = 'white')
     plt.ylabel('匯率', fontsize = 11, color = 'white')
     plt.title('【週 K 線】 與 【模型預測分布】 對照圖', fontsize = 14, 
